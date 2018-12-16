ca5a5ec2bb9e9d1eefc366dfb55912b60003ee7b
hbidani <hassan_bidani@hotmail.de>
2018-12-17 00:30:24 +0100

    Refact: removed unused param

--- src/main/java/de/royal/bipro/BiproWsTurbineSteamServiceApplication.java
+++ src/main/java/de/royal/bipro/BiproWsTurbineSteamServiceApplication.java
@@ -2,12 +2,10 @@ package de.royal.bipro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
 @SpringBootApplication
 @EnableTurbineStream
-@EnableDiscoveryClient
 public class BiproWsTurbineSteamServiceApplication {
 
 	public static void main(String[] args) {
